a101470a36bd1d6dc5f834c5b43244b7aa4ff2e8
sillas.soares <sillas.soares@emiolo.com>
2014-07-28 09:28:02 -0300

    creating repository

---
+++ scripts/field_registry.php
@@ -0,0 +1,11 @@
+<?php
+namespace Aura\View;
+return array_merge(
+    require __DIR__ . '/input_registry.php',
+    [
+        'radios'     => function () { return new Helper\Form\Radios(new Helper\Form\Input\Checked); },
+        'checkboxes' => function () { return new Helper\Form\Checkboxes(new Helper\Form\Input\Checked); },
+        'select'     => function () { return new Helper\Form\Select; },
+        'textarea'   => function () { return new Helper\Form\Textarea; },
+    ]
+);
